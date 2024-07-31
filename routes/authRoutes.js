9c274076bd304bdf1ff7c340774227e3916f5201
Akhilesh_the_developer <117347141+Akh-ile-sh@users.noreply.github.com>
2024-07-31 11:46:07 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,8 @@
 import express from "express";
-import { login, register } from "../controller/authController.js";
+import { login } from "../controller/authController.js";
 
 const router = express.Router();
 
-router.route("/register").post(register);
 router.post("/login", login);
 
 export default router;
